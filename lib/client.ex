9ab199e77fb6c9860b4115fe9f11f3ab46161270
Cristian <cristianames.92@gmail.com>
2016-06-20 17:58:36 -0300

    Client

--- lib/client.ex
+++ lib/client.ex
@@ -4,19 +4,44 @@ defmodule ChatClient do
 
 	use GenServer 
 
-	def send_unicast(client) do 
-	end 
-
-	def send_broadcast(client, message) do
+	def silence(client, server, to_be_silenced) do
+		GenServer.call(server, {:silence, client, to_be_silenced})
 	end
 
-	def receive_unicast(client, server, {msgid, sender, message}) do 
-	end 
+	def unsilence(client, to_be_unsilenced) do
+		GenServer.call(server, {:unsilence, client, to_be_unsilenced})
+	end
 
-	def receive_broadcast(client, server, {sender, message}) do 
-	end 
+	def receive_message_started(client, server, sender) do
+		
+	end
 
-	def message_started(client, server, sender) do 
-	end 
+	def send_unicast(client, destination, message) do
+		
+	end
+
+	def receive_unicast_received(client, server, localid) do
+		
+	end
+
+	def receive_unicast_readed(client, server, localid) do
+		
+	end
+
+	def send_broadcast(client, message) do
+		
+	end
+
+	def receive_unicast(client, server, {message_id, message}) do
+		
+	end
+
+	def receive_broadcast(client, server, message) do
+		
+	end
+
+	def read_unicast(client, message_id) do
+		
+	end
 
 end 
